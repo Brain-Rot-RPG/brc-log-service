5c0e57fca1de58860e07a48cec21e2082dc812b2
Mesrop <mesropmkl@gmail.com>
2026-01-30 15:09:17 +0100

    feat: app containerization

--- src/application/server.ts
+++ src/application/server.ts
@@ -12,10 +12,14 @@ app.use(express.json());
 const file  = fs.readFileSync(require.resolve('../../docs/log.yaml'), 'utf8');
 const swaggerDocument = YAML.parse(file);
 
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+if (process.env.NODE_ENV !== 'production') {
+    app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+}
 
 const port = process.env.PORT || 4010;
 app.listen(port, () => {
     logger.info(`Server listening on http://localhost:${port}`);
-    logger.info(`Swagger docs at http://localhost:${port}/docs`);
+    if (process.env.NODE_ENV !== 'production') {
+        logger.info(`Swagger docs at http://localhost:${port}/docs`);
+    }
 });
