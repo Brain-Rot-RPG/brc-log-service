f2d081ba47417668e8839aad7a108a37e88e921b
Mesrop <mesropmkl@gmail.com>
2026-01-30 12:04:50 +0100

    feat: init npm

---
+++ src/application/server.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import * as fs from "node:fs";
+import * as YAML from 'yaml';
+import swaggerUi from 'swagger-ui-express';
+
+const app = express();
+app.use(express.json());
+
+const file  = fs.readFileSync(require.resolve('../../docs/log.yaml'), 'utf8')
+const swaggerDocument = YAML.parse(file)
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+const port = process.env.PORT || 4010;
+app.listen(port, () => {
+    console.log(`Server listening on http://localhost:${port}`);
+    console.log(`Swagger docs at http://localhost:${port}/docs`);
+});
