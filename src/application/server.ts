ece708b5582b4506c76944bb6021a916a886edfe
Mesrop <mesropmkl@gmail.com>
2026-01-30 13:47:27 +0100

    feat: add strict linter

--- src/application/server.ts
+++ src/application/server.ts
@@ -1,13 +1,14 @@
+import * as fs from 'node:fs';
+
 import express from 'express';
-import * as fs from "node:fs";
-import * as YAML from 'yaml';
 import swaggerUi from 'swagger-ui-express';
+import * as YAML from 'yaml';
 
 const app = express();
 app.use(express.json());
 
-const file  = fs.readFileSync(require.resolve('../../docs/log.yaml'), 'utf8')
-const swaggerDocument = YAML.parse(file)
+const file  = fs.readFileSync(require.resolve('../../docs/log.yaml'), 'utf8');
+const swaggerDocument = YAML.parse(file);
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
