e857379988ea82c4e9c2ea2c735b1b0d15ac57df
Mesrop <mesropmkl@gmail.com>
2026-02-02 15:45:14 +0100

    feat: implement domain

---
+++ src/errors/ValidationError.ts
@@ -0,0 +1,8 @@
+import {BaseError} from './BaseError.js';
+
+export class ValidationError extends BaseError {
+    constructor(message: string) {
+        super(message, 400);
+        this.name = 'ValidationError';
+    }
+}
